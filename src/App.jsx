514979d549d1b6f607d583c424705528874d28e8
Andrew-george5 <115142674+Andrew-george5@users.noreply.github.com>
2024-04-14 22:07:53 +0200

    first update after making the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
-import Services from "./components/Services";
+import Button from "./components/Button";
 
-const App = () => {
+export default function App() {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer />
+      <h1>hello world</h1>
+      <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          something
+        </Button>
       </div>
-
       <ButtonGradient />
     </>
   );
-};
-
-export default App;
+}
